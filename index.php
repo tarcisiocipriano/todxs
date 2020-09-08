59a47332fbc96146f5e57c172e967352f8d5a7dc
Tarcisio Cipriano <creativecipriano@gmail.com>
2020-09-08 03:50:19 -0300

    implement posts list in the index template

--- index.php
+++ index.php
@@ -19,17 +19,31 @@ get_header(); ?>
         <div class="row">
           <?php
             if( have_posts() ):
-              while( have_posts() ): the_post();
-                ?>
-                  <article>
-                    <h2><?php the_title(); ?></h2>
-                    <div><?php the_content(); ?></div>
-                  </article>
-                <?php
-              endwhile;
-            else:
-          ?>
-            <p>Nothing to display.</p>
+              while( have_posts() ): the_post(); ?>
+                <article <?php post_class(); ?>>
+                  <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                  <a href="<?php the_permalink(); ?>"">
+                    <div class="post-thumbnail">
+                      <?php if( has_post_thumbnail() ):
+                        the_post_thumbnail( 'todxs-blog', array( 'class' => 'img-fluid' ) );
+                      endif; ?>
+                    </div>
+                  </a>
+                  <div class="meta">
+                    <p>Postado por <?php the_author_posts_link(); ?> em <?php echo get_the_date(); ?></p>
+                    <br />
+                    <?php if( has_category() ): ?>
+                      Categorias: <span><?php the_category( ' ' ) ?></span>
+                    <?php endif; ?>
+                    <br />
+                    <?php if( has_tag() ): ?>
+                      Tags: <span><?php the_tags( '', ', ' ) ?></span>
+                    <?php endif; ?>
+                  </div>
+                  <div><?php the_excerpt(); ?></div>
+                </article>
+            <?php endwhile; else: ?>
+            <p>Nenhuma postagem.</p>
           <?php endif; ?>
         </div>
       </div>
