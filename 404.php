456281b6566d853839a4c16b5c42078aa01d601a
Tarcisio Cipriano <creativecipriano@gmail.com>
2020-09-08 07:07:09 -0300

    refactory 404 page template

--- 404.php
+++ 404.php
@@ -1,6 +1,21 @@
-<?php get_header(); ?>
-<div class="container d-flex flex-column align-items-center">
-  <h1><?php echo '404 not found' ?></h1>
-  <div style="width: 400px; margin: -100px 0;" id="pride"></div>
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ * 
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ */
+get_header(); ?>
+<div class="content-area">
+  <main>
+    <div class="container">
+      <div class="error-404">
+        <div class="d-flex flex-column align-items-center">
+          <h1>Página não encontrada</h1>
+          <a href="<?php echo home_url( '/' ); ?>" style="width: 400px; margin: -50px 0;" id="pride"></a>
+          <p>A página que você tentou acessar, não existe nesse site...</p>
+        </div>
+      </div>
+    </div>
+  </main>
 </div>
 <?php get_footer(); ?>
