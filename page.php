15a51af1c58ba3e81fe51af1c526d1de1b44078b
Tarcisio Cipriano <creativecipriano@gmail.com>
2020-09-08 04:19:26 -0300

    refactor page template

--- page.php
+++ page.php
@@ -17,20 +17,12 @@ get_header(); ?>
     <main>
       <div class="container">
         <div class="row">
-          <?php
-            if( have_posts() ):
-              while( have_posts() ): the_post();
-                ?>
-                  <article class="col">
-                    <h1><?php the_title(); ?></h1>
-                    <div><?php the_content(); ?></div>
-                  </article>
-                <?php
-              endwhile;
-            else:
-          ?>
-            <p>Nothing to display.</p>
-          <?php endif; ?>
+          <?php while( have_posts() ): the_post(); ?>
+            <article class="col">
+              <h1><?php the_title(); ?></h1>
+              <div><?php the_content(); ?></div>
+            </article>
+          <?php endwhile; ?>
         </div>
       </div>
     </main>
